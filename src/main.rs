f67284ee754bc02da956f58f36bf3594580d8e52
Marco Amann <marco@pfudi.de>
2020-06-05 13:13:49 +0200

    working with new buffers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::env;
 use std::fs::File;
-use std::io::{BufReader, BufRead, Error, ErrorKind};
+use std::io::{BufReader, BufRead, Error, ErrorKind, Read};
+use std::convert::TryInto;
+
+use rayon::prelude::*;
 
 /**
     A struct holding a 9x9 filed sudoku board
@@ -71,15 +74,27 @@ fn is_slice_unique(sl: &[u8;9]) -> bool{
      ) == (1u16<<9) -1
 }
 
-fn eval_sudoku_string(line: String) -> bool{
-
+fn eval_sudoku_string(sudoku: Sudoku) -> bool{
+    let correct_range = sudoku.fields.iter().all(|e| *e<10 && *e>0);
+    if !correct_range { return false;}
+    is_valid_view(sudoku.as_columns())
+        && is_valid_view(sudoku.as_rows())
+        && is_valid_view(sudoku.as_boxes())
 }
-fn parse_raw_line_to_sudoku(input: Result<String,_>) -> Option<Sudoku>{
-    match input{
-        Ok(input) => {
-            input
-            Sudoku
-        }
+
+fn parse_raw_line_to_sudoku(input: &str) -> Option<Sudoku>{
+    let mut fields =  [0;81];
+    let count = input.split(',')
+        .filter_map(|split| split.trim().parse::<u8>().ok())
+        .take(81)
+        .filter(|e| *e<10 && *e>0)
+        .enumerate()
+        .map(|(idx,num)| fields[idx] = num)
+        .count();
+    match count {
+        81 => {
+            Some(Sudoku{ fields })
+        },
         _ => None
     }
 }
@@ -87,53 +102,35 @@ fn parse_raw_line_to_sudoku(input: Result<String,_>) -> Option<Sudoku>{
 fn main() -> std::io::Result<()>  {
 
     let input_file = env::args()
-        .skip(1)
-        .next()
-        .unwrap();
-        //.ok_or(|| ErrorKind::InvalidInput)?;
-
+        .nth(1)
+        .ok_or(Error::from(ErrorKind::InvalidInput))?;
 
     let file = File::open(input_file)?;
-    let reader = BufReader::new(file);
+    let mut reader = BufReader::new(file);
 
 
+    let mut lines = String::new();
+    reader.read_to_string(&mut lines).unwrap();
+
+
+    let valids = lines.par_lines()
+        .filter_map(|line| parse_raw_line_to_sudoku(line) )
+        .map(|sudoku| eval_sudoku_string(sudoku) )
+        .filter(|b|*b)
+        .count();
+
+
+
+    /*
     let valids = reader.lines()
-        .enumerate()
-        .filter_map(|(i,line)|parse_raw_line_to_sudoku(line))
-        .map(|(i,line)| (i,eval_sudoku_string(line)) )
-        //.map(|(i,validity)| {println!("Line {} \t valid: {}",i,validity)});
-        .fold(0,|acc,t| acc+t.1 as i32);
+        .filter_map(|line|line.ok())
+        .filter_map(|line| parse_raw_line_to_sudoku(&line) )
+        .map(|sudoku| eval_sudoku_string(sudoku) )
+        .filter(|b|*b)
+        .count();
+     */
 
-    println!("Valids: {}",valids);
 
-    let sudoku = Sudoku{
-        fields:[
-            8, 3, 5,  4, 1, 6,  9, 2, 7,
-            2, 9, 6,  8, 5, 7,  4, 3, 1,
-            4, 1, 7,  2, 9, 3,  6, 5, 8,
-
-            5, 6, 9,  1, 3, 4,  7, 8, 2,
-            1, 2, 3,  6, 7, 8,  5, 4, 9,
-            7, 4, 8,  5, 2, 9,  1, 6, 3,
-
-            6, 5, 2,  7, 8, 1,  3, 9, 4,
-            9, 8, 1,  3, 4, 5,  2, 7, 6,
-            3, 7, 4,  9, 6, 2,  8, 1, 5
-        ]
-    };
-
-    let correct_range = sudoku.fields.iter().all(|e| *e<10 && *e>0);
-    if !correct_range {
-        eprintln!("Invalid range");
-        //return Err();
-    }
-
-    let valid_columns = is_valid_view(sudoku.as_columns());
-    let valid_rows = is_valid_view(sudoku.as_rows());
-    let valid_boxes = is_valid_view(sudoku.as_boxes());
-
-    println!("Valid placement: {}", valid_rows && valid_columns && valid_rows);
-
-    println!("Hello, world!");
+    println!("Valid sudokus: {}",valids);
     Ok(())
 }
